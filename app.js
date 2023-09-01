48ef3f4215d97e3f6b0bc57dfd8cefff68221cf0
vertigo97 <askulic@student.ius.edu.ba>
2023-09-01 11:04:38 +0200

    learning tyscript

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+}
+var number1 = 5;
+var number2 = 3.5;
+var res = add(number1, number2);
+console.log(res);
---
+++ app.ts
@@ -0,0 +1,11 @@
+function add(a: number, b: number) {
+    return a + b;
+}
+
+
+const number1 = 5;
+const number2 = 3.5;
+
+
+const res = add(number1, number2);
+console.log(res);
